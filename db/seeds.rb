197a782532c6c327a6a5079276f203fe5b371abb
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-10 16:18:52 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurant_params = [
+  { name: "Dishoom",
+    address: "7, Edgeware Road",
+    phone_number: "44 207 345 234",
+    category: "chinese"
+  },
+
+  { name: "Cafe Rouge",
+    address: "75, Kilburn High Road",
+    phone_number: "44 207 367 278",
+    category: "french"
+  },
+
+  { name: "Nobu",
+    address: "54, Oxford Street",
+    phone_number: "44 208 315 200",
+    category: "japanese"
+  },
+
+  { name: "Bella Italia",
+    address: "8, Belzise Park",
+    phone_number: "44 208 385 268",
+    category: "italian"
+  },
+
+  { name: "Yo Sushi",
+    address: "35, Oxford Street",
+    phone_number: "44 248 317 210",
+    category: "japanese"
+  }
+
+]
+
+Restaurant.create!(restaurant_params)
